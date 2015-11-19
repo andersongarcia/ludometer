64315beb349dece187191da211f2bfe06201a6b9
Leandro <leandro.cp@gmail.com>
2015-11-19 15:47:55 -0200

    Implementação de leitura e gravação de parâmetros no SharedPreferences pela TimerActivity.

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/ludometer/util/LudometerPreferences.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/ludometer/util/LudometerPreferences.java
@@ -6,14 +6,25 @@ import android.content.SharedPreferences;
 
 /**
  * Created by lcp on 19/11/15.
+ *
+ * Classe auxiliar para leitura e gravação de SharedPreferences.
  */
 public class LudometerPreferences {
 
     private final static String PREF_NAME = "LudometerPreferences";
     private final static String TIMER_START_TIME = "TimerStartTime";
 
+    static SharedPreferences getSp (Activity activity) {
+        return activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
     public static long getTimerPreference (Activity activity) {
-        SharedPreferences sp = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        return sp.getLong(TIMER_START_TIME,30000);
+        return getSp(activity).getLong(TIMER_START_TIME, 30000);
+    }
+
+    public static void setTimerPreference (Activity activity, long startTime) {
+        SharedPreferences.Editor ed = getSp(activity).edit();
+        ed.putLong(TIMER_START_TIME, startTime);
+        ed.commit();
     }
 }
